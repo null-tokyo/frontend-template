9092fc28eb15b354c297144ac7075dc6836fe04d
yusuke omi <ysk.omi0818@gmail.com>
2018-06-08 17:58:56 +0900

    eslintの追加

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # frontend-template
+
+## Commands
+
+### watch
+```
+$ npm run start
+```
+### build by development mode
+```
+$ npm run dev
+```
+### build by production mode
+```
+$ npm run prod
+```
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,38 @@ module.exports = {
     mode: 'development',
     entry: './src/js/index.js',
     output: {
-        path: `${__dirname}/dist/js`,
-        filename: 'main.js'
+        filename: 'main.js',
+        path: `${__dirname}/dist/js`
     },
     module: {
         rules: [
+            //eslint
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'eslint-loader',
+                        options: {
+                            quiet: true,
+                            failOnWarning: true
+                        }
+                    }
+                ]
+            },
+            //babel
             {
                 test: /\.js$/,
                 use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['env', {'modules': false}]
-                        ]
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['env', {'modules': false} ]
+                            ]
+                        }
                     }
-                }
                 ]
             }
         ]      
